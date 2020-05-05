3273eb093bbd2c952a17d91add0c3c0607fceec3
frankkode <masasimpafra@gmail.com>
2020-05-06 00:04:02 +0200

    add sendmailjs

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,25 +1,17 @@
 function sendMail(contactForm) {
-    emailjs.send("gmail", "dog service", {
+    emailjs.send("frankmasabo_gmail_com", "dog_service", {
             "from_name": contactForm.name.value,
+            "from_subject": contactForm.subject.value,
             "from_email": contactForm.emailaddress.value,
-            "form_subject": contactForm.subject.value,
-            "information_request": contactForm.informationsummary.value
+            "service_request": contactForm.servicesummary.value
         })
         .then(
             function (response) {
                 console.log("SUCCESS", response);
-                alert("Your message has been sent successfully");
-                document.getElementById('id01').reset();
             },
             function (error) {
                 console.log("FAILED", error);
-                alert("Message was not sent");
-                document.getElementById('id01').reset();
             }
         );
     return false; // To block from loading a new page
 }
-/* google email key */
-(function () {
-    emailjs.init("user_PfKdr4g0Xy587kbdqzLqb");
-})();
