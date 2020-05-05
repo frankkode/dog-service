4e5770453acc7b3ad1bcd2a6fc2f0e0224eeed6b
frankkode <masasimpafra@gmail.com>
2020-05-05 22:31:32 +0200

    add send email test script

---
+++ assets/js/tests/sendMailTest.js
@@ -0,0 +1,7 @@
+sendMail = function (message) {
+    if (message === "SUCCESS") {
+        return "Your message has been sent successfully";
+    } else if (message === "FAILED") {
+        return "Message was not sent";
+    }
+};
